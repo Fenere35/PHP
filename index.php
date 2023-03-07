ad85bdd0d3ca8c2dd264c56bdb0239682252b3c5
Shmakov Daniil <daniilshmakov04@gmail.com>
2023-03-07 11:49:36 +0300

    готово 1 лаба

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TheFirstLaba</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <header class="header">
+        <div class="wrapper header__flex">
+            <a href="#" class="logo__link"><img src="/img/Logo.png" alt="Логотип московского политеха" class="logo__img"></a>
+            <h2 class="title">TheFirstLaba</h2>
+        </div>
+    </header>
+
+    <main>
+        <div class="wrapper centre">
+            <?php echo 'Привет, мир!'; ?>
+        </div>
+    </main>
+
+    <footer>
+        <div class="wrapper centre">
+            <?php echo 'Шмаков Даниил Олегович 221-322'; ?>
+        </div>
+    </footer>
+</body>
+</html>
